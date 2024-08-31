d445c6740986f668bc0ae6c17fbacbe02034e1b6
Vincent Kipngetich <vinniebluz@icloud.com>
2024-08-31 06:17:50 +0300

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Vincent Lang’at on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
