352df7d8b11210dbfdf28ae3a40acfa3ba951a30
Vincent Kipngetich <vinniebluz@icloud.com>
2024-09-10 14:25:27 +0300

    Added text modified - minimum scale factor incase the text don't fit the screen

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,22 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageString = "When the genius bar needs help, they call you!"
     var body: some View {
         VStack {
-            Image(systemName: "4.circle")
-                .resizable()
-                .scaledToFit()
-            Text("Version 3.0")
+            Text(messageString)
                 .font(.largeTitle)
-                .fontWeight(.black)
-            Rectangle()
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .foregroundColor(.cyan)
+                .padding()
+                .frame(width:150, height: 100)
+                .border(.orange, width: 1)
+            HStack{
+                Button("Awesome"){
+                    messageString = "You are awesome!"
+                }.buttonStyle(.borderedProminent)
+                Button("Great"){
+                    messageString = "You are Great!"
+                }.buttonStyle(.borderedProminent)
+            }
         }
-        .padding()
-        .foregroundColor(.purple)
+        //        .padding()
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
